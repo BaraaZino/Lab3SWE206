185b7379d840535c19c131ae16ec66a4d90a1de4
M3ZH2 <mtsat2005@gmail.com>
2024-09-17 14:53:18 +0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,14 @@ public class Main {
         a = 10;
         b = 5;
         System.out.println(divide(a, b));
+        System.out.println(multiply(a, b));
     }
 
     public static int divide(int a, int b){
         return a/b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a*b;
     }
 }
