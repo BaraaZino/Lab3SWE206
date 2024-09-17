a3bc97422868216f75bb5d07eafb5e491278936b
M3ZH2 <mtsat2005@gmail.com>
2024-09-17 14:48:06 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = 5;
+        System.out.println(divide(a, b));
+    }
+
+    public static int divide(int a, int b){
+        return a/b;
+    }
+}
